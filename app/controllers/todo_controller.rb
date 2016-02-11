a91d43ad8f1cdf6567df38073801564ad20abf2c
Amodu Temitope <amodut@gmail.com>
2016-02-11 17:44:03 +0100

    setup blog app

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,8 +1,37 @@
 class TodoController < Petite::Controller
   def index
-    @name = "buhari"
+    @todo = Todo.select_all
+  end
+
+  def new
+
   end
 
   def create
+    todo_params = params
+    @todo = Todo.new
+    @todo.title = todo_params["title"]
+    @todo.body = todo_params["body"]
+    @todo.created_at = Time.now.to_s
+    @todo.save
+    redirect_to "/"
   end
+
+  def edit
+    @todo = Todo.find(params["id"])
+  end
+
+  def update
+    @todo = Todo.find(params["id"])
+    @todo.title = params["title"]
+    @todo.body = params["body"]
+    @todo.save
+    redirect_to "/"
+  end
+
+  def destroy
+    Todo.delete(params["id"])
+    redirect_to "/"
+  end
+
 end
